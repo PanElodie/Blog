eb76da261997d745ab9b232b4a7c88ec19db48ff
AmelieRbls <79206671+AmelieRbls@users.noreply.github.com>
2021-11-18 12:02:57 +0100

    Update article.php

--- article.php
+++ article.php
@@ -7,45 +7,28 @@
     <title>Billet</title>
 </head>
 <body>
-<h1>Comment créer une barre de recherche sur son site avec PHP et MySQL</h1>
-<p>Salut! Mbote la famille! Suite à vos demandes sur comment créer une barre de recherche sur son site, j'ai trouvé bon de vous écrire cet article pour répondre et aider tout celui qui souhaite créer une barre de recherche sur son site internet.
+<h1>Qu'est ce que le php ?</h1>
+<h2> le 18/11/2021 </h2>
+<p>PHP (officiellement, ce sigle est un acronyme récursif pour PHP Hypertext Preprocessor) est un langage de scripts généraliste et Open Source, spécialement conçu pour le développement d'applications web. Il peut être intégré facilement au HTML.</p>
 
-Vous aurez dans les jours qui viennent ce tutoriel en PDF et vidéo. Pour aujourd’hui, lisez-le quand même.
+<p>Ce qui distingue PHP des langages de script comme le Javascript, est que le code est exécuté sur le serveur, générant ainsi le HTML, qui sera ensuite envoyé au client. Le client ne reçoit que le résultat du script, sans aucun moyen d'avoir accès au code qui a produit ce résultat. Vous pouvez configurer votre serveur web afin qu'il analyse tous vos fichiers HTML comme des fichiers PHP. Ainsi, il n'y a aucun moyen de distinguer les pages qui sont produites dynamiquement des pages statiques.</p>
 
-Il est très important d'ajouter une barre de recherche sur son site internet car celle-ci va faciliter les visiteurs de votre site de trouver facilement et rapidement ce qu'ils cherchent depuis votre site.
+<p>Le grand avantage de PHP est qu'il est extrêmement simple pour les néophytes, mais offre des fonctionnalités avancées pour les experts. Ne craignez pas de lire la longue liste de fonctionnalités PHP. Vous pouvez vous plonger dans le code, et en quelques instants, écrire des scripts simples.</p>
+ 
+<h3>écrit par MJ </h3>
 
-Si je parle de barre de recherche sur votre site, je parlerai de moteur de recherche pour des grands sites tels que Google, Facebook, Twitter, Amazon. Ces grands sites utilisent des algorithmes un peu plus complexes que ceux dont j'utilise dans ce tutoriel.
-
-Pourquoi une barre de recherche et non pas un moteur de recherche?
-Comme je viens de le dire, je préfère utiliser le terme simple barre de recherche que de dire moteur de recherche. On laisse le terme moteur de recherche pour des grands sites et focalisons-nous sur la barre de recherche qui, son rôle est de trier les articles contenant le terme de la requête de l'internaute.
-
-Je dois être très simple dans ce tutoriel pour qu'on comprenne ensemble comment créer une barre de recherche sur son site internet.
-
-Les avantages d'une barre de recherche sur son site web
-Je ne peux pas seulement vous montrer comment créer cet outil sur votre site web sans que vous puissiez connaître les avantages qu'elle offre à votre site.
-
-Une barre de recherche sur votre site est très importante car:
-
-Elle facilite une bonne navigation des internautes sur votre site;
-Elle vous permet d'avoir une idée sur ce que veulent les visiteurs de votre site;
-Elle vivifie votre site;
-Grâce elle, votre site peut être apprécié par les robots d'indexation.
-Les connaissances nécessaires pour suivre ce tutoriel
-Pour pouvoir être en mesure de créer une barre de recherche sur votre site internet, ou pour suivre ce tutoriel avec toute facilité, vous devez avoir quelques connaissances nécessaire en :
-
-PHP : il s'agira durant ce tutoriel de traiter les données d'un formulaire et vous le savez mieux que moi, pour traiter les données d'un formulaire, il faut utiliser le langage PHP.
-MySQL : les données qu'on va traiter se trouveront dans une base de données et seront stockées aussi dans cette même base de données. Une connaissance en MySQL(PDO) serait très importante pour suivre ce tutoriel.
-HTML : avec le HTML, on aura à créer le formulaire qui sera la base de cette barre de recherche.
-Si vous avez toutes ces connaissances, je suis sûr que vous allez comprendre ce tutoriel sans problème.
-
-Sinon si vous avez quelques-unes de ces connaissances, je vous demande de suivre le tutoriel avec attention et à chaque fois que vous aurez des difficultés, utilisez l'espace commentaire de notre site pour exposer votre problème et il y aura une personne de bonne foi qui vous aidera.
-
-Sinon, vous devez soit fermer ce tutoriel et allez apprendre les langages cités ci-haut, soit continuer à lire ce tutoriel en se forçant de comprendre et à chaque fois que vous rencontrerez des difficultés, l'espace commentaire est là pour vous aider.
-
-Je n'ai pas parlé de CSS parce que la mise en forme de cette barre de recherche dépend du design de votre site. Améliorez-la selon votre site.
-
-Maintenant que vous avez l'idée sur ce qu'il vous faut pour continuer ce tutoriel, je pense qu'on peut déjà se lancer. Bonne chance et bonne compréhension à tous!</p>
 
 </body>
-
+<style>
+    h1{
+        text-align: center;
+    }
+    p{
+        width:50%;
+        margin-left:25%;
+    }
+    h2, h3{
+        margin-left:65%;
+    }
+</style>
 </html>
